7968a3016be666ff25884df473f81cf8f7806c7f
Taha Saif <tahasaifsurhio454@gmail.com>
2025-09-12 17:29:06 +0500

    code uploaded

---
+++ -TRADING-ADMIN-PANEL/src/pages/Dasboard.jsx
@@ -0,0 +1,4 @@
+export default function Dashboard() {
+    return <h1>Admin Dashboard 1</h1>;
+  }
+  
---
+++ -TRADING-ADMIN-PANEL/src/pages/Investments.jsx
@@ -0,0 +1,4 @@
+export default function Investments() {
+    return <h1>Admin Dashboard 2 - Investments</h1>;
+  }
+  
